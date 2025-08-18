6f3ef4b8cb5805048aa25eb0bffad1de5d2a48a8
mvlassis <manos.vlassis@canonical.com>
2025-08-18 16:10:02 +0200

    Create docstring

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -4,6 +4,20 @@ import jubilant
 
 
 def get_application_data(juju: jubilant.Juju, app_name: str, relation_name: str) -> dict:
+    """Retrieves the application data from a specific relation.
+
+    Args:
+        juju: The Juju client object used to execute CLI commands.
+        app_name: The name of the Juju application.
+        relation_name: The name of the relation endpoint to query.
+
+    Returns:
+        A dictionary containing the application data for the specified relation.
+
+    Raises:
+        ValueError: If no relation data can be found for the specified
+            relation endpoint.
+    """
     unit_name = f"{app_name}/0"
     command_stdout = juju.cli("show-unit", unit_name, "--format=json")
     result = json.loads(command_stdout)
