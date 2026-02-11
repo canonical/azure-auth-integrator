2c2913fa3838f17bb89f5da815bdebaf78dafcf8
mvlassis <manos.vlassis@canonical.com>
2026-02-11 12:02:29 +0200

    Update azure-auth-integrator

--- src/events/lifecycle.py
+++ src/events/lifecycle.py
@@ -11,9 +11,16 @@ from constants import AZURE_SERVICE_PRINCIPAL_RELATION_NAME
 from core.context import Context
 from events.base import BaseEventHandler
 from lib.azure_service_principal import (
-    AzureServicePrincipalProviderData,
-    AzureServicePrincipalProviderEventHandlers,
-    ServicePrincipalInfoRequestedEvent,
+    AzureServicePrincipalProviderModel,
+    AzureServicePrincipalRequirerModel,
+)
+from charms.data_platform_libs.v1.data_interfaces import (
+    DataContractV1,
+    RequirerCommonModel,
+    ResourceProviderEventHandler,
+    ResourceProviderModel,
+    ResourceRequestedEvent,
+    build_model
 )
 from utils.logging import WithLogging
 
@@ -27,19 +34,14 @@ class LifecycleEvents(BaseEventHandler, WithLogging):
         self.charm = charm
         self.context = context
 
-        self.azure_service_principal_provider_data = AzureServicePrincipalProviderData(
-            self.charm.model, AZURE_SERVICE_PRINCIPAL_RELATION_NAME
-        )
-        self.azure_service_principal_provider = AzureServicePrincipalProviderEventHandlers(
-            self.charm, self.azure_service_principal_provider_data
-        )
+        self.azure_service_principal_provider = ResourceProviderEventHandler(self.charm, AZURE_SERVICE_PRINCIPAL_RELATION_NAME, RequirerCommonModel)
 
         self.framework.observe(self.charm.on.update_status, self._on_update_status)
         self.framework.observe(self.charm.on.config_changed, self._on_config_changed)
         self.framework.observe(self.charm.on.secret_changed, self._on_secret_changed)
         self.framework.observe(
-            self.azure_service_principal_provider.on.service_principal_info_requested,
-            self._on_azure_service_principal_info_requested,
+            self.azure_service_principal_provider.on.resource_requested,
+            self._on_azure_service_principal_resource_requested,
         )
 
     def _on_update_status(self, event: ops.UpdateStatusEvent):
@@ -48,7 +50,7 @@ class LifecycleEvents(BaseEventHandler, WithLogging):
 
     def _on_config_changed(self, event: ConfigChangedEvent) -> None:  # noqa: C901
         """Event handler for configuration changed events."""
-        # Only execute in the unit leader
+        # Only execute in the leader unit
         if not self.charm.unit.is_leader():
             return
 
@@ -78,20 +80,42 @@ class LifecycleEvents(BaseEventHandler, WithLogging):
     def _update_provider_data(self):
         """Update the contents of the relation data bag."""
         if (
-            len(self.azure_service_principal_provider_data.relations) > 0
+            len(self.azure_service_principal_provider.relations) > 0
             and self.context.azure_service_principal
         ):
-            for relation in self.azure_service_principal_provider_data.relations:
-                self.azure_service_principal_provider_data.update_relation_data(
-                    relation.id, self.context.azure_service_principal.to_dict()
-                )
+            self.logger.info("Updating the provider data.")
+            data = self.context.azure_service_principal.to_dict()
+            self.logger.info(data)
+            for relation in self.azure_service_principal_provider.relations:
+                model = build_model(self.azure_service_principal_provider.interface.repository(
+                    relation.id, relation.app), DataContractV1[AzureServicePrincipalProviderModel])
+                self.logger.info(model)
+                for request in model.requests:
+                    self.logger.info(request)
+                    request.subscription_id=data["subscription-id"]
+                    request.tenant_id=data["tenant-id"]
+                    for key in ("client-id", "client-secret"):
+                        setattr(request, key, data[key])
+                        
+                self.azure_service_principal_provider.interface.write_model(relation.id, model)
 
-    def _on_azure_service_principal_info_requested(
-        self, event: ServicePrincipalInfoRequestedEvent
-    ):
-        """Handle the `service-principal-info-requested` event."""
-        self.logger.info("On service-principal-info-requested")
+    def _on_azure_service_principal_resource_requested(self, event: ResourceRequestedEvent):
+        """Handle the data_interfaces `resource requested` event."""
+        self.logger.info("On resource-requested")
         if not self.charm.unit.is_leader():
             return
 
         self._update_provider_data()
+        if not self.context.azure_service_principal:
+            return
+        data = self.context.azure_service_principal.to_dict()
+        response = AzureServicePrincipalProviderModel(
+            salt=event.request.salt,
+            request_id=event.request.request_id,
+            resource="azure-service-principal",
+            subscription_id=data["subscription-id"],
+            tenant_id=data["tenant-id"],
+            client_id=data["client-id"],
+            client_secret=data["client-secret"]
+        )
+        self.azure_service_principal_provider.set_response(event.relation.id, response)
--- tests/integration/test-charm-azure-service-principal/src/charm.py
+++ tests/integration/test-charm-azure-service-principal/src/charm.py
@@ -10,22 +10,14 @@ the azure service principal requires-provides relation.
 
 import logging
 
-from ops import Relation
 from ops.charm import ActionEvent, CharmBase, RelationJoinedEvent
 from ops.main import main
 from ops.model import ActiveStatus, BlockedStatus
 
-from charms.data_platform_libs.v1.data_interfaces import (
-    DataContractV1,
-    RequirerCommonModel,
-    RequirerDataContractV1,
-    ResourceCreatedEvent,
-    ResourceRequirerEventHandler,
-    build_model
-)
-from lib.azure_service_principal_v1 import (
+from lib.azure_service_principal import (
     AzureServicePrincipalRequirer,
-    AzureServicePrincipalProviderModel,
+    ServicePrincipalInfoChangedEvent,
+    ServicePrincipalInfoGoneEvent
 )
 
 logger = logging.getLogger(__name__)
@@ -43,28 +35,17 @@ class ApplicationCharm(CharmBase):
         # Default charm events.
         self.framework.observe(self.on.start, self._on_start)
 
-        requests = [
-            RequirerCommonModel(resource="azure-service-principal"),
-        ]
         self.azure_service_principal_client = AzureServicePrincipalRequirer(self, RELATION_NAME)
-        
-        self.azure_service_principal_client = ResourceRequirerEventHandler(self, RELATION_NAME,
-                                                                           requests,
-                                                                           response_model=AzureServicePrincipalProviderModel)
 
-        # self.framework.observe(
-        #     self.azure_service_principal_client.on.service_principal_info_changed,
-        #     self._on_service_principal_info_changed,
-        # )
+        self.framework.observe(
+            self.azure_service_principal_client.on.service_principal_info_changed,
+            self._on_service_principal_info_changed,
+        )
         self.framework.observe(self.on[RELATION_NAME].relation_joined, self._on_relation_joined)
-        self.framework.observe(self.azure_service_principal_client.on.resource_created,
-                                       self._on_resource_created)
-        self.framework.observe(self.azure_service_principal_client.on.resource_entity_created,
-                               self._on_resource_entity_created)
-        # self.framework.observe(
-        #     self.azure_service_principal_client.on.service_principal_info_gone,
-        #     self._on_service_principal_info_gone,
-        # )
+        self.framework.observe(
+            self.azure_service_principal_client.on.service_principal_info_gone,
+            self._on_service_principal_info_gone,
+        )
         self.framework.observe(self.on.update_status, self._on_update_status)
         self.framework.observe(
             self.on.get_azure_service_principal_info_action, self._on_get_service_principal_info
@@ -79,63 +60,32 @@ class ApplicationCharm(CharmBase):
         logger.info("azure-service-principal-credentials relation joined...")
         self.unit.status = ActiveStatus()
 
-    def _on_resource_created(self, event: ResourceCreatedEvent) -> None:
-        logger.info("New relation resource created.")
-        response = event.response
-        info = {
-            "subscription-id" : response.subscription_id,
-            "tenant-id" : response.tenant_id,
-            "client-id" : response.client_id,
-            "client-secret" : response.client_secret
-        }
-        logger.info(info)
-        
-    def _on_resource_entity_created(self, event: ResourceCreatedEvent) -> None:
-        logger.info("New resource entity created.")
-        response = event.response
-        info = {
-            "subscription-id" : response.subscription_id,
-            "tenant-id" : response.tenant_id,
-            "client-id" : response.client_id,
-            "client-secret" : response.client_secret
-        }
-        logger.info(info)
+    def _on_service_principal_info_changed(self, e: ServicePrincipalInfoChangedEvent):
+        service_principal_info = (
+            self.azure_service_principal_client.get_azure_service_principal_info()
+        )
+        if service_principal_info:
+            logger.debug(f"Credentials changed. New credentials: {service_principal_info}")
 
-        
-    # def _on_service_principal_info_changed(self, e: ServicePrincipalInfoChangedEvent):
-    #     service_principal_info = (
-    #         self.azure_service_principal_client.get_azure_service_principal_info()
-    #     )
-    #     if service_principal_info:
-    #         logger.debug(f"Credentials changed. New credentials: {service_principal_info}")
-
-    # def _on_service_principal_info_gone(self, _: ServicePrincipalInfoGoneEvent):
-    #     logger.debug("Credentials gone...")
-    #     self.unit.status = BlockedStatus("Waiting for relation.")
+    def _on_service_principal_info_gone(self, _: ServicePrincipalInfoGoneEvent):
+        logger.debug("Credentials gone...")
+        self.unit.status = BlockedStatus("Waiting for relation.")
 
     def _on_update_status(self, _):
-        pass
-        # service_principal_info = (
-        #     self.azure_service_principal_client.get_azure_service_principal_info()
-        # )
-        # if service_principal_info:
-        #     logger.debug(f"Azure service principal client info: {service_principal_info}")
+        service_principal_info = (
+            self.azure_service_principal_client.get_azure_service_principal_info()
+        )
+        if service_principal_info:
+            logger.debug(f"Azure service principal client info: {service_principal_info}")
 
     def _on_get_service_principal_info(self, event: ActionEvent):
-        if not self.azure_service_principal_relation:
-            return
-        requests = build_model(self.azure_service_principal_client.interface.repository(self.azure_service_principal_relation.id,
-                                                                self.azure_service_principal_relation.app),
-                        DataContractV1[AzureServicePrincipalProviderModel]).requests
-        logger.info(requests)
+        service_principal_info = (
+            self.azure_service_principal_client.get_azure_service_principal_info()
+        )
+        if service_principal_info:
+            event.set_results(service_principal_info)
+            logger.debug(f"Azure service principal client info: {service_principal_info}")
 
-    @property
-    def azure_service_principal_relation(self) -> Relation | None:
-        """Return the azure_service_principal relation if present."""
-        if not hasattr(self, "azure_service_principal_client"):
-            logger.debug("No azure_service_principal relation client")
-            return None
-        return self.azure_service_principal_client.relations[0] if len(self.azure_service_principal_client.relations) else None
 
 if __name__ == "__main__":
     main(ApplicationCharm)
